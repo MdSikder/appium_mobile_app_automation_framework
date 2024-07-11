9cf04f83b3a52b73ac72af4740db0f760988131a
MdSikder <mdporagbsmrstu@gmail.com>
2024-07-11 14:43:45 +0600

    Load configuration from YAML file

---
+++ config/config.py
@@ -0,0 +1,7 @@
+class Config:
+    APPIUM_SERVER_URL = "http://localhost:4723/wd/hub"
+    PLATFORM_NAME = "Android"
+    DEVICE_NAME = "YourDeviceName"
+    APP_PACKAGE = "com.example.yourapp"
+    APP_ACTIVITY = ".MainActivity"
+    TIMEOUT = 30
